196d1cffe973e83a9a2fd08dc77dcfce71a6caf0
Alex Ameen <alex.ameen.tx@gmail.com>
2024-10-25 09:22:17 -0500

    inline tag_list_item.ui junk

--- taggui/widgets/tag_list_item.py
+++ taggui/widgets/tag_list_item.py
@@ -1,20 +1,51 @@
-from PySide6.QtWidgets import QWidget
-
-from ui_tag_list_item import Ui_TagListItem
+from PySide6.QtCore import QCoreApplication, QMetaObject
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QSizePolicy
+from PySide6.QtWidgets import QSpacerItem, QToolButton
 
 class TagListItem(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.ui = Ui_TagListItem()
-        self.ui.setupUi(self)
-        self.ui.closeButton.clicked.connect(self.closeButtonClicked)
+        if not self.objectName():
+            self.setObjectName("Tag")
+        self.resize(250, 60)
+
+        self.horizontalLayout = QHBoxLayout(self)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.label = QLabel(self)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.label.setText(
+            QCoreApplication.translate("TagListItem", "TextLabel", None)
+        )
+
+        self.horizontalSpacer = QSpacerItem(
+            40, 20,
+            QSizePolicy.Policy.Expanding,
+            QSizePolicy.Policy.Minimum
+        )
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.deleteButton = QToolButton(self)
+        self.deleteButton.setObjectName("deleteButton")
+        self.horizontalLayout.addWidget(self.deleteButton)
+        self.deleteButton.setText(
+            QCoreApplication.translate("TagListItem", "x", None)
+        )
+        self.deleteButton.clicked.connect(self.deleteButtonClicked)
+
+        self.setWindowTitle(
+            QCoreApplication.translate("TagListItem", "Form", None)
+        )
+
+        QMetaObject.connectSlotsByName(self)
 
     def setText(self, text):
-        self.ui.label.setText(text)
+        self.label.setText(text)
 
     def getText(self):
-        return self.ui.label.text()
+        return self.label.text()
 
-    def closeButtonClicked(self):
+    def deleteButtonClicked(self):
         self.parent.removeItem(self.getText())
