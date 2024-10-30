4ea5dcf119ad3c66bd333654538087787b795416
Alex Ameen <alex.ameen.tx@gmail.com>
2024-10-30 06:00:12 -0500

    create util.py

--- v2/tag_area_widget.py
+++ v2/tag_area_widget.py
@@ -11,15 +11,7 @@ from PySide6.QtWidgets import (
 )
 
 from flow_layout import FlowLayout
-
-def deduplicate_list(input_list) -> list[any]:
-    seen = set()  # Create a set to keep track of seen elements
-    deduplicated_list = []  # List to hold deduplicated elements
-    for item in input_list:
-        if item not in seen:  # Check if the item is already seen
-            seen.add(item)  # Add the item to the seen set
-            deduplicated_list.append(item)  # Append it to the result list
-    return deduplicated_list
+from util import deduplicate_list
 
 class TagAreaWidget(QWidget):
     def __init__(self, tags=[]) -> None:
