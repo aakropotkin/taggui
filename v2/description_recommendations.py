10f12c7f7c4ebadc8834450f8b5d8c5a3af0d1ba
Alex Ameen <alex.ameen.tx@gmail.com>
2024-11-02 06:53:56 -0500

    set min length of 32

--- v2/description_recommendations.py
+++ v2/description_recommendations.py
@@ -29,7 +29,11 @@ class Describer():
             image_path = Path(image_path)
         image = Image.open(image_path)
         inputs = self.processor(images=image, return_tensors="pt")
-        outputs = self.model.generate(**inputs, max_new_tokens=512)
+        outputs = self.model.generate(
+            **inputs,
+            min_length=32,
+            max_new_tokens=512
+        )
         caption = self.processor.decode(outputs[0], skip_special_tokens=True)
         return caption
 
